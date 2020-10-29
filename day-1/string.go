1882aa5c8d144be65e745d6e2d1d830684586f4e
Fahmi <65661058+fahmi1597@users.noreply.github.com>
2020-10-30 01:12:44 +0700

    Day 1

---
+++ day-1/string.go
@@ -0,0 +1,14 @@
+
+package main
+
+import ("fmt")
+
+func main(){
+	fmt.Println("Fahmi")
+	fmt.Println("Jamaludin")
+	fmt.Println("Mas'ud")
+	fmt.Println(len("Fahmi"))
+	fmt.Println(("Jamaludin"[0]))
+	fmt.Println(("Mas'ud"[2]))
+
+}
---
+++ day-1/variableadv.go
@@ -0,0 +1,14 @@
+package main
+
+import ("fmt")
+
+func main(){
+
+	var (
+		firstName = "Mikun"
+		lastName = "Fahmi"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
